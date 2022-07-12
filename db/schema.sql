1849b55a3db1e4e5ad834bbb20c3136fb554ec0f
Jessica Sanchez <contact.jessicasanchez@gmail.com>
2022-07-12 10:28:11 -0400

    added some sql schemas

--- db/schema.sql
+++ db/schema.sql
@@ -5,16 +5,26 @@ USE employees;
 
 CREATE TABLE department (
   -- id set to ainteger, automatic increment, and primary key
+  id INT NOT NULL
+  AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
   -- name set to varchar, max size 30, not null
 );
 
 CREATE TABLE role (
   -- id set to integer, automatic increment, and primary key
+  id INT NOT NULL
+  AUTO_INCREMENT PRIMARY KEY
   -- title set to varchar, max size 30, and not null
+  role_title VARCHAR(30) NOT NULL
   -- salary set to decimal and not null
+  salary FLOAT NOT NULL
   -- department id set to integer and not null
+  department_id INT NOT NULL
   -- foreign key department id referencing department table on id with on delete constraint
-
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (

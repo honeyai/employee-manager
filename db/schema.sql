bc9920650468ccf5b3e17c665a0b9a684a17b72e
honeyai <contact.jessicasanchez@gmail.com>
2022-07-12 10:56:28 -0400

    finished the schema

--- db/schema.sql
+++ db/schema.sql
@@ -29,11 +29,22 @@ CREATE TABLE role (
 
 CREATE TABLE employee (
   -- id set to integer with automatic increment and primary key constraints
+  id INT
+  AUTO_INCREMENT PRIMARY KEY
   -- first name set to var char, max size 30, and not null contraint
+  first_name VARCHAR(30) NOT NULL
   -- last name set to var char, max size 30, and not null
+  last_name VARCHAR(30) NOT NULL
   -- role id set to integer and not null
+  role_id INT NOT NULL
   -- manager id set to integer
+  manager_id INT
   -- foreign key on role id referencing role table on id with on delete constraint
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
   -- foreign key on manager id referencing employee table on id with on delete constraint
-
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
